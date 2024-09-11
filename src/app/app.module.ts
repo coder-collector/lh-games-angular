68be1a4057b2cab10809cfc0821e6bd80ae6ed14
Filipe Santos <filiper233@gmail.com>
2024-09-11 00:05:12 -0300

    Segunda Atividade

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,21 @@ import {MatInputModule} from '@angular/material/input';
 import {MatToolbarModule} from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RestritoRoutingModule } from './restrito/restrito-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { InicioComponent } from './inicio/inicio.component';
 import { LoginComponent } from './login/login.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { RestritoComponent } from './restrito/restrito.component';
+import { AtualizaProdutoComponent } from './restrito/atualiza-produto/atualiza-produto.component';
+import { CadastroProdutoComponent } from './restrito/cadastro-produto/cadastro-produto.component';
+import { ListaProdutoComponent } from './restrito/lista-produto/lista-produto.component';
+//import { MenuProdutoComponent } from './restrito/menu-produto/menu-produto.component';
+import { MenuRestritoComponent } from './restrito/menu-restrito/menu-restrito.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -24,7 +33,13 @@ import { RodapeComponent } from './rodape/rodape.component';
     InicioComponent,
     LoginComponent,
     MenuComponent,
-    RodapeComponent
+    RodapeComponent,
+    RestritoComponent,
+    AtualizaProdutoComponent,
+    CadastroProdutoComponent,
+    ListaProdutoComponent,
+   // MenuProdutoComponent,
+    MenuRestritoComponent
   ],
   imports: [
     BrowserModule,
@@ -36,7 +51,11 @@ import { RodapeComponent } from './rodape/rodape.component';
     MatGridListModule,
     MatIconModule,
     MatInputModule,
-    MatToolbarModule
+    MatToolbarModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RestritoRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
